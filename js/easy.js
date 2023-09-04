690b0bf8af99d2935c3c1c2eb4d03d1a1256352f
tolufashina <fashinatoluwa@gmail.com>
2023-09-04 00:35:33 -0700

    second commit

---
+++ js/easy.js
@@ -0,0 +1,13 @@
+/* 
+Filter out companies which have more than one 'o' without the filter method
+
+*/
+
+const itcompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+// console.log(itcompanies.join(" "));
+
+// console.log(itcompanies.indexOf("oo"));
+
+// console.log(itcompanies.slice(0,3));
+// const fash =(itcompanies.indexOf(itcompanies[Math.floor(itcompanies.length/2)]));
